3b44b85acc2ebb1499ed9dd7f02a906adb4b7132
Anshul Sahni <anshul_sahni@live.com>
2017-10-16 01:27:09 +0530

    added some more tests to check for text being rendered

--- src/modules/pages/Home/components/tests/HomeScreen.test.js
+++ src/modules/pages/Home/components/tests/HomeScreen.test.js
@@ -5,8 +5,13 @@ import { expect } from 'chai';
 import HomeScreen from '../HomeScreen';
 
 describe('<HomeScreen /> Render', () => {
-  it('checks for right text being rendered', () => {
+  it('checks for single div being rendered', () => {
     const wrapper = shallow(<HomeScreen />);
     expect(wrapper.find('div')).to.have.length(1);
+  });
+
+  it('checks for text being rendered', () => {
+    const wrapper = shallow(<HomeScreen />);
+    expect(wrapper.text()).to.be.equal('Hello World !');
   });
 });
