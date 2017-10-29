784874b943a5e02a6bb3a7cc57d6017e338d4508
Anshul Sahni <anshul_sahni@live.com>
2017-10-30 01:13:16 +0530

    added container for Home

--- src/modules/pages/Home/index.js
+++ src/modules/pages/Home/index.js
@@ -1,5 +1,17 @@
 import { connect } from 'react-redux';
 
+import { fetchUser } from './actions';
+
 import HomeScreen from './components/HomeScreen';
 
-export default connect()(HomeScreen);
+const mapStateToProps = ({ home: { networkBusy } }) => ({
+  loading: networkBusy,
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchRandomUserDetails: () => {
+    dispatch(fetchUser());
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomeScreen);
