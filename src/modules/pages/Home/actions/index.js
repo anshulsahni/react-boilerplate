6f21ddedd3a5dee1f8f837554d85c2ae66ed188a
Anshul Sahni <anshul_sahni@live.com>
2017-10-30 01:12:00 +0530

    added actions for Home

---
+++ src/modules/pages/Home/actions/index.js
@@ -0,0 +1,20 @@
+import { FETCH_USER, CHANGE_NETWORK_BUSY_STATUS, SET_ERROR, LOAD_USER } from '../constants';
+
+export const fetchUser = () => ({
+  type: FETCH_USER
+});
+
+export const changeNetworkBusyStatus = networkBusy => ({
+  type: CHANGE_NETWORK_BUSY_STATUS,
+  networkBusy,
+});
+
+export const setError = error => ({
+  type: SET_ERROR,
+  error,
+});
+
+export const loadUser = user => ({
+  type: LOAD_USER,
+  user,
+});
